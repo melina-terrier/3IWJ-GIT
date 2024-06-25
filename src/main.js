b7753abfbf9608432a7a1ae276ce538e6432a8cf
“Mélina <mterrier4@myges.fr>
2024-06-25 22:13:49 +0200

    test d'erreur

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 document.querySelector('button').addEventListen('click', () => {
-  alert('Bienvenue sur Spotify!')
+  alert(Bienvenue sur Spotify!)
 });

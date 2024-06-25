7d5f1d6fe7cdff3fec9cda5ca07e30061a0a4c96
“Mélina <mterrier4@myges.fr>
2024-06-25 21:50:06 +0200

    test d'erreur

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module'
-    },
-    rules: {
-        // Ajoutez vos règles ESLint ici
+    languageOptions: {
+        parserOptions: {
+            ecmaVersion: 12,
+            sourceType: 'module'
+        },
     }
 };
